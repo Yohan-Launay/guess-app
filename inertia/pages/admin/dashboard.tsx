398f8eb21b0dd4a921fb55ae8308759d5fdabd8e
yohanlaunay <yohandeveloppe@gmail.com>
2024-04-02 19:10:59 +0200

    User dashboard

--- inertia/pages/admin/dashboard.tsx
+++ inertia/pages/admin/dashboard.tsx
@@ -1,7 +1,8 @@
-export default function Dashboard() {
+export default function Dashboard({ user }: { user: string | null }) {
   return (
     <div>
       <h1>Dashboard</h1>
+      {user.email && <p>Bienvenue {user.email}</p>}
     </div>
   )
 }

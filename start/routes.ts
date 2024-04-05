557fb83fdc5e0ddae7df926a4a3ecbb5e04001e2
yohanlaunay <yohandeveloppe@gmail.com>
2024-04-05 18:32:13 +0200

    routes

--- start/routes.ts
+++ start/routes.ts
@@ -13,31 +13,35 @@ import { middleware } from '#start/kernel'
 const HomeController = () => import('#controllers/home_controller')
 const SessionController = () => import('#controllers/session_controller')
 
-router.get('/', [HomeController, 'index'])
-
-// Home artist page
-router.get('/guess', [ArtistsController, 'index'])
-
-// Login and logout
-router.get('/login', async ({ inertia }) => {
-  return inertia.render('auth/login')
-})
-router.post('login', [SessionController, 'store'])
-
-router.post('logout', async ({ auth, response }) => {
-  await auth.use('web').logout()
-  response.redirect('/login')
+router.group(() => {
+  // Home page and guess
+  router.get('/', [HomeController, 'index'])
+  router.get('/guess', [ArtistsController, 'index'])
 })
 
-// Dashboard
 router
-  .get('dashboard', ({ auth, inertia }) => {
-    const user = auth.getUserOrFail()
-    console.log(user)
-    return inertia.render('admin/dashboard', { user })
+  .group(() => {
+    // Admin dashboard
+    router.get('dashboard', ({ auth, inertia }) => {
+      const user = auth.getUserOrFail()
+      console.log(user)
+      return inertia.render('admin/dashboard', { user })
+    })
   })
   .use(
     middleware.auth({
       guards: ['web'],
     })
   )
+
+router.group(() => {
+  router.get('/login', async ({ inertia }) => {
+    return inertia.render('auth/login')
+  })
+  router.post('login', [SessionController, 'store'])
+
+  router.post('logout', async ({ auth, response }) => {
+    await auth.use('web').logout()
+    response.redirect('/login')
+  })
+})
